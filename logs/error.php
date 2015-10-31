4f7cbc66cf0d80b4df7c966357007baf645d254a
spikart <zyuralex@gmail.com>
2015-10-31 13:14:37 +0200

    new

---
+++ logs/error.php
@@ -0,0 +1,20 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2013-10-14 08:00:41 UTC
+#Software: Joomla Platform 12.2.0 Stable [ Neil Armstrong ] 21-September-2012 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2013-10-14T08:00:41+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2013-10-14T08:07:58+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2013-10-14T08:10:06+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2013-10-17T09:17:33+00:00	INFO	joomlafailure	Empty password not allowed
+2013-10-24T04:30:21+00:00	INFO	joomlafailure	Empty password not allowed
+2013-10-24T05:56:44+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2013-11-02T03:53:34+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2013-11-02T03:53:38+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2013-11-07T03:26:25+00:00	INFO	joomlafailure	Empty password not allowed
+2013-11-15T03:41:40+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2013-11-15T03:41:47+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2013-11-15T08:59:58+00:00	INFO	joomlafailure	The two factor authentication Secret Key is invalid.
+2014-06-06T04:20:55+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
+2015-10-28T19:37:43+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
---
+++ logs/joomla_update.php
@@ -0,0 +1,103 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2013-11-08 03:44:45 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2013-11-08T03:44:45+00:00	INFO	update	Finalising installation.
+2013-11-08T03:44:45+00:00	INFO	update	Ran query from file 3.2.0. Query text: /* Core 3.2 schema updates */  ALTER TABLE `#__content_types` ADD COLUMN `conten.
+2013-11-08T03:44:45+00:00	INFO	update	Ran query from file 3.2.0. Query text: UPDATE `#__content_types` SET `content_history_options` = '{"formFile":"administ.
+2013-11-08T03:44:45+00:00	INFO	update	Ran query from file 3.2.0. Query text: UPDATE `#__content_types` SET `content_history_options` = '{"formFile":"administ.
+2013-11-08T03:44:45+00:00	INFO	update	Ran query from file 3.2.0. Query text: UPDATE `#__content_types` SET `content_history_options` = '{"formFile":"administ.
+2013-11-08T03:44:45+00:00	INFO	update	Ran query from file 3.2.0. Query text: UPDATE `#__content_types` SET `content_history_options` = '{"formFile":"administ.
+2013-11-08T03:44:45+00:00	INFO	update	Ran query from file 3.2.0. Query text: UPDATE `#__content_types` SET `content_history_options` = '{"formFile":"administ.
+2013-11-08T03:44:45+00:00	INFO	update	Ran query from file 3.2.0. Query text: UPDATE `#__content_types` SET `content_history_options` = '{"formFile":"administ.
+2013-11-08T03:44:45+00:00	INFO	update	Ran query from file 3.2.0. Query text: INSERT INTO `#__content_types` (`type_title`, `type_alias`, `table`, `rules`, `f.
+2013-11-08T03:44:45+00:00	INFO	update	Ran query from file 3.2.0. Query text: UPDATE `#__extensions` SET `params` = '{"template_positions_display":"0","upload.
+2013-11-08T03:44:45+00:00	INFO	update	Ran query from file 3.2.0. Query text: UPDATE `#__extensions` SET `params` = '{"lineNumbers":"1","lineWrapping":"1","ma.
+2013-11-08T03:44:45+00:00	INFO	update	Ran query from file 3.2.0. Query text: INSERT INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `folder`.
+2013-11-08T03:44:45+00:00	INFO	update	Ran query from file 3.2.0. Query text: INSERT INTO `#__menu` (`menutype`, `title`, `alias`, `note`, `path`, `link`, `ty.
+2013-11-08T03:44:46+00:00	INFO	update	Ran query from file 3.2.0. Query text: ALTER TABLE `#__modules` ADD COLUMN `asset_id` INT(10) UNSIGNED NOT NULL DEFAULT.
+2013-11-08T03:44:46+00:00	INFO	update	Ran query from file 3.2.0. Query text: CREATE TABLE `#__postinstall_messages` (   `postinstall_message_id` bigint(20) u.
+2013-11-08T03:44:46+00:00	INFO	update	Ran query from file 3.2.0. Query text: INSERT INTO `#__postinstall_messages` (`extension_id`, `title_key`, `description.
+2013-11-08T03:44:46+00:00	INFO	update	Ran query from file 3.2.0. Query text: CREATE TABLE IF NOT EXISTS `#__ucm_history` (   `version_id` int(10) unsigned NO.
+2013-11-08T03:44:46+00:00	INFO	update	Ran query from file 3.2.0. Query text: ALTER TABLE `#__users` ADD COLUMN `otpKey` varchar(1000) NOT NULL DEFAULT '' COM.
+2013-11-08T03:44:47+00:00	INFO	update	Ran query from file 3.2.0. Query text: ALTER TABLE `#__users` ADD COLUMN `otep` varchar(1000) NOT NULL DEFAULT '' COMME.
+2013-11-08T03:44:47+00:00	INFO	update	Ran query from file 3.2.0. Query text: CREATE TABLE IF NOT EXISTS `#__user_keys` (   `id` int(10) unsigned NOT NULL AUT.
+2013-11-08T03:44:47+00:00	INFO	update	Ran query from file 3.2.0. Query text: /* Update bad params for two cpanel modules */  UPDATE `#__modules` SET `params`.
+2013-11-08T03:44:47+00:00	INFO	update	Deleting removed files and folders.
+2013-11-08T03:44:48+00:00	INFO	update	Cleaning up after installation.
+2013-11-08T03:44:48+00:00	INFO	update	Update to version 3.2.0 is complete.
+2013-12-19T04:26:44+00:00	INFO	update	Update started by user Super User (321). Old version is 3.2.0.
+2013-12-19T04:26:44+00:00	INFO	update	Downloading update file from .
+2013-12-19T04:26:49+00:00	INFO	update	File Joomla_3.2.x_to_3.2.1-Stable-Patch_Package.zip successfully downloaded.
+2013-12-19T04:26:49+00:00	INFO	update	Starting installation of new version.
+2013-12-19T04:26:51+00:00	INFO	update	Finalising installation.
+2013-12-19T04:26:51+00:00	INFO	update	Ran query from file 3.2.1. Query text: DELETE FROM `#__postinstall_messages` WHERE `title_key` = 'PLG_USER_JOOMLA_POSTI.
+2013-12-19T04:26:52+00:00	INFO	update	Deleting removed files and folders.
+2013-12-19T04:26:53+00:00	INFO	update	Cleaning up after installation.
+2013-12-19T04:26:53+00:00	INFO	update	Update to version 3.2.1 is complete.
+2014-02-17T02:07:35+00:00	INFO	update	Update started by user Super User (321). Old version is 3.2.1.
+2014-02-17T02:07:35+00:00	INFO	update	Downloading update file from .
+2014-02-17T02:08:19+00:00	INFO	update	File Joomla_3.2.1_to_3.2.2-Stable-Patch_Package.zip successfully downloaded.
+2014-02-17T02:08:20+00:00	INFO	update	Starting installation of new version.
+2014-02-17T02:08:22+00:00	INFO	update	Finalising installation.
+2014-02-17T02:08:23+00:00	INFO	update	Ran query from file 3.2.2-2013-12-22. Query text: ALTER TABLE `#__update_sites` ADD COLUMN `extra_query` VARCHAR(1000) DEFAULT '';.
+2014-02-17T02:08:23+00:00	INFO	update	Ran query from file 3.2.2-2013-12-22. Query text: ALTER TABLE `#__updates` ADD COLUMN `extra_query` VARCHAR(1000) DEFAULT '';.
+2014-02-17T02:08:23+00:00	INFO	update	Ran query from file 3.2.2-2013-12-28. Query text: UPDATE `#__menu` SET `component_id` = (SELECT `extension_id` FROM `#__extensions.
+2014-02-17T02:08:23+00:00	INFO	update	Ran query from file 3.2.2-2014-01-08. Query text: INSERT INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `folder`.
+2014-02-17T02:08:23+00:00	INFO	update	Ran query from file 3.2.2-2014-01-15. Query text: INSERT INTO `#__postinstall_messages` (`extension_id`, `title_key`, `description.
+2014-02-17T02:08:23+00:00	INFO	update	Ran query from file 3.2.2-2014-01-18. Query text: /* Update updates version length */ ALTER TABLE `#__updates` MODIFY `version` va.
+2014-02-17T02:08:23+00:00	INFO	update	Ran query from file 3.2.2-2014-01-23. Query text: INSERT INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `folder`.
+2014-02-17T02:08:23+00:00	INFO	update	Deleting removed files and folders.
+2014-02-17T02:08:23+00:00	INFO	update	Cleaning up after installation.
+2014-02-17T02:08:23+00:00	INFO	update	Update to version 3.2.2 is complete.
+2014-03-10T01:38:54+00:00	INFO	update	Update started by user Super User (321). Old version is 3.2.2.
+2014-03-10T01:38:54+00:00	INFO	update	Downloading update file from .
+2014-03-10T01:39:47+00:00	INFO	update	File Joomla_3.2.2_to_3.2.3-Stable-Patch_Package.zip successfully downloaded.
+2014-03-10T01:39:47+00:00	INFO	update	Starting installation of new version.
+2014-03-10T01:39:50+00:00	INFO	update	Finalising installation.
+2014-03-10T01:39:50+00:00	INFO	update	Ran query from file 3.2.3-2014-02-20. Query text: UPDATE `#__extensions` ext1, `#__extensions` ext2 SET ext1.`params` =  ext2.`par.
+2014-03-10T01:39:50+00:00	INFO	update	Deleting removed files and folders.
+2014-03-10T01:39:51+00:00	INFO	update	Cleaning up after installation.
+2014-03-10T01:39:51+00:00	INFO	update	Update to version 3.2.3 is complete.
+2014-06-03T09:25:00+00:00	INFO	update	Update started by user Super User (681). Old version is 3.2.3.
+2014-06-03T09:25:00+00:00	INFO	update	Downloading update file from .
+2014-06-03T09:30:07+00:00	INFO	update	Update started by user Super User (681). Old version is 3.2.3.
+2014-06-03T09:30:07+00:00	INFO	update	File Joomla_3.3.0-Stable-Update_Package.zip successfully downloaded.
+2014-06-03T09:30:08+00:00	INFO	update	Starting installation of new version.
+2014-06-03T09:31:18+00:00	INFO	update	Finalising installation.
+2014-06-03T09:31:18+00:00	INFO	update	Ran query from file 3.3.0-2014-02-16. Query text: ALTER TABLE `#__users` ADD COLUMN `requireReset` tinyint(4) NOT NULL DEFAULT 0 C.
+2014-06-03T09:31:18+00:00	INFO	update	Ran query from file 3.3.0-2014-04-02. Query text: INSERT INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `folder`.
+2014-06-03T09:31:18+00:00	INFO	update	Deleting removed files and folders.
+2014-06-03T09:31:21+00:00	INFO	update	Cleaning up after installation.
+2014-06-03T09:31:21+00:00	INFO	update	Update to version 3.3.0 is complete.
+2014-06-03T09:31:27+00:00	INFO	update	File Joomla_3.3.0-Stable-Update_Package.zip successfully downloaded.
+2014-09-12T02:08:08+00:00	INFO	update	Update started by user Super User (681). Old version is 3.3.0.
+2014-09-12T02:08:09+00:00	INFO	update	Downloading update file from .
+2014-09-12T02:09:04+00:00	INFO	update	File Joomla_3.3.x_to_3.3.3-Stable-Patch_Package.zip successfully downloaded.
+2014-09-12T02:09:06+00:00	INFO	update	Starting installation of new version.
+2014-09-12T02:09:24+00:00	INFO	update	Finalising installation.
+2014-09-12T02:09:25+00:00	INFO	update	Deleting removed files and folders.
+2014-09-12T02:09:33+00:00	INFO	update	Cleaning up after installation.
+2014-09-12T02:09:33+00:00	INFO	update	Update to version 3.3.3 is complete.
+2015-01-14T03:26:15+00:00	INFO	update	Update started by user Super User (681). Old version is 3.3.3.
+2015-01-14T03:26:16+00:00	INFO	update	Downloading update file from .
+2015-01-14T03:30:02+00:00	INFO	update	File Joomla_3.3.x_to_3.3.6-Stable-Patch_Package.zip successfully downloaded.
+2015-01-14T03:30:03+00:00	INFO	update	Starting installation of new version.
+2015-01-14T03:30:08+00:00	INFO 127.0.0.1	update	Finalising installation.
+2015-01-14T03:30:09+00:00	INFO 127.0.0.1	update	Ran query from file 3.3.4-2014-08-03. Query text: ALTER TABLE `#__user_profiles` CHANGE `profile_value` `profile_value` TEXT NOT N.
+2015-01-14T03:30:09+00:00	INFO 127.0.0.1	update	Ran query from file 3.3.6-2014-09-30. Query text: INSERT INTO `#__update_sites` (`name`, `type`, `location`, `enabled`) VALUES ('J.
+2015-01-14T03:30:09+00:00	INFO 127.0.0.1	update	Ran query from file 3.3.6-2014-09-30. Query text: INSERT INTO `#__update_sites_extensions` (`update_site_id`, `extension_id`) VALU.
+2015-01-14T03:30:10+00:00	INFO 127.0.0.1	update	Deleting removed files and folders.
+2015-01-14T03:30:15+00:00	INFO 127.0.0.1	update	Cleaning up after installation.
+2015-01-14T03:30:15+00:00	INFO 127.0.0.1	update	Update to version 3.3.6 is complete.
+2015-06-08T02:22:45+00:00	INFO ::1	update	COM_JOOMLAUPDATE_UPDATE_LOG_DELETE_FILES
+2015-10-28T19:38:19+00:00	INFO 127.0.0.1	update	Update started by user Super User (681). Old version is 3.4.1.
+2015-10-28T19:38:20+00:00	INFO 127.0.0.1	update	Downloading update file from https://github.com/joomla/joomla-cms/releases/download/3.4.5/Joomla_3.4.x_to_3.4.5-Stable-Patch_Package.zip.
+2015-10-28T19:39:00+00:00	INFO 127.0.0.1	update	File Joomla_3.4.x_to_3.4.5-Stable-Patch_Package.zip successfully downloaded.
+2015-10-28T19:39:00+00:00	INFO 127.0.0.1	update	Starting installation of new version.
+2015-10-28T19:39:55+00:00	INFO 127.0.0.1	update	Finalising installation.
+2015-10-28T19:39:55+00:00	INFO 127.0.0.1	update	Deleting removed files and folders.
+2015-10-28T19:40:02+00:00	INFO 127.0.0.1	update	Cleaning up after installation.
+2015-10-28T19:40:02+00:00	INFO 127.0.0.1	update	Update to version 3.4.5 is complete.
